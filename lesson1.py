7533a97b58bda5c5b1910f44487a4c016951fd5e
Gentrit14 <113131403+Gentrit14@users.noreply.github.com>
2024-10-26 12:43:09 +0200

    ...

---
+++ lesson1.py
@@ -0,0 +1,70 @@
+
+
+
+teperature = 22.0
+
+name = "Gentrit"
+
+print(type(teperature))
+print(type(name))
+
+age = 16
+
+x = 5
+
+y = 8
+
+result = x + y
+
+print(result)
+
+age  = age + 1
+print(age)
+
+first_name = "Gentrit"
+last_name = "Sinani"
+
+fullname = first_name + " " + last_name
+print(fullname)
+
+
+list1 = ["item1" , "item2", "item3"]
+
+
+shopping_list = ["apples" , "bananas" ,  "milk" , 3 ,4.5]
+
+print(shopping_list)
+
+
+homework = ["math" , "psikologji" , "biologji" , "anglisht"]
+print(homework[0])
+print(homework[3])
+print(homework[1])
+
+to_do_list = ["teach 110" , "Go shopping" , "Create the app" , "check devops"]
+first_item = to_do_list[0]
+second_item = to_do_list[1]
+third_item = to_do_list[2]
+
+favorite_subject = ["psikologji" , "sociologjia" , "ed.fizike" , "frangjisht"]
+forth_subject = favorite_subject[3]
+print(forth_subject)
+
+shopping_list.append("strawberries")
+
+print(shopping_list)
+
+shopping_list.insert( 2 , "lemons" )
+print(shopping_list)
+
+to_do_list.remove("check devops")
+print(to_do_list)
+
+del to_do_list [2]
+print(to_do_list)
+
+to_do_list[0] = "teach 10"
+
+print(to_do_list)
+
+
